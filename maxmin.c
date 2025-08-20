22f9c4187596ae5dcbe4e94c8d845a91670ac5db
Krish Srivastava <krishsrivastava2006@gmail.com>
2025-08-21 00:09:49 +0530

    Maxmin
    FInding the maximum and minimum value of an non-dynamic array of integers.

---
+++ maxmin.c
@@ -0,0 +1,24 @@
+// Finding maximum and minimum in an array.
+
+#include <stdio.h>
+
+int main() {
+    int arrray[] = {1, 2, 3, 4};
+    size_t size = sizeof(arrray) / sizeof(arrray[0]);// We have the length here
+    int max = 0;
+    int min = 0;
+    int tman;
+    for (int i = 0; i < size;i++) {
+        if (arrray[i] > max) {
+            tman = max;
+            max = i;
+            if(tman < min) {
+                min = tman;
+            }
+        } else if(arrray[i] < max && arrray[i] < min) {
+            min = i;
+        }
+    }
+    printf("The maximum value number is: %d\n", arrray[max]);
+    printf("The minimum value number is: %d", arrray[min]);
+}
